a4c351a5be027130162b480b2ae4c1d3e440b4ad
Chris Franklin <cf73021@gmail.com>
2018-07-19 16:01:05 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,6 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli << name
   katz_deli.each_with_index do |name, i|
     i = i+1
     name = "Welcome, #{name}. You are number #{i} in line."
