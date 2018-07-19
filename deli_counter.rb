9f49cc7b9c928671efb036b6d7a0b73c1002cf5e
Chris Franklin <cf73021@gmail.com>
2018-07-19 15:44:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,18 @@
 # Write your code here.
 katz_deli = []
 
-def line()
+def line(katz_deli)
+  if katz_deli.length == 0
+    return "There is nobody waiting to be served!"
+  else
+    return "The line is currently: "
 end
+
 def take_a_number(katz_deli, name)
-  katz_deli = katz_deli << name
-  return katz_deli[-1]
+  katz_indexed = []
+  katz_deli.each_with_index do |name, i|
+    i = i+1
+    name = "Welcome, #{name}. You are number #{i} in line."
+    katz_indexed << name
+  return katz_indexed[-1]
 end
