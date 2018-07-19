f4d2100b287ad395aa7b2f713f5e9d6abc5b405f
Chris Franklin <cf73021@gmail.com>
2018-07-19 16:38:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,6 +25,7 @@ end
 def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
+  end
   shifted = katz_deli.shift()
   puts "Currently serving " + shifted
 end
